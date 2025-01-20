14eba5dd6e100bac3d1a6ba662bd4255dbc81da5
Andrey <zxtorii@gmail.com>
2025-01-20 18:48:55 +0800

    Player

---
+++ Chess/Player.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace ChessLogic
+{
+    public enum Player
+    {
+        None,
+        White,
+        Black
+    }
+
+
+    public static class PlayerExtensions
+    {
+        public static Player GetOpponent(this Player player)
+        {
+            switch (player)
+        }
+    }
+}
