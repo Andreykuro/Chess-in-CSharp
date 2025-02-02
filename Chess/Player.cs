910d93551f1e37ed5f2c7e3a67d3d166ca096f11
Andrey <zxtorii@gmail.com>
2025-02-02 14:05:16 +0800

    Add files via upload

---
+++ Chess/Player.cs
@@ -0,0 +1,32 @@
+﻿namespace ChessLogic
+{
+    public class Player
+    {
+        public PieceColor Color { get; private set; }
+        public bool IsHuman { get; private set; }
+        private ChessBot bot;
+
+        public Player(PieceColor color, bool isHuman, string difficulty = "easy")
+        {
+            Color = color;
+            IsHuman = isHuman;
+            if (!isHuman)
+            {
+                bot = new ChessBot(difficulty);
+            }
+        }
+
+        public Move MakeMove(Board board)
+        {
+            if (IsHuman)
+            {
+                // Handle human move input (likely from UI)
+                throw new NotImplementedException("Human move logic not implemented.");
+            }
+            else
+            {
+                return bot.MakeMove(board, Color);
+            }
+        }
+    }
+}
