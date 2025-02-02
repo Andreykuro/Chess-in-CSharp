b6adb6bdb3419770d5652c111b7c7e70c7d2f4cc
Andrey <zxtorii@gmail.com>
2025-02-02 14:07:01 +0800

    Add files via upload

---
+++ Chess/ChessUI/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows;
+using ChessLogic;
+namespace ChessUI
+{
+    public partial class MainWindow : Window
+    {
+        private Board board;
+        private Player humanPlayer;
+        private Player botPlayer;
+        private PieceColor currentPlayer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeGame();
+        }
+
+        private void InitializeGame()
+        {
+            board = new Board();
+            humanPlayer = new Player(PieceColor.White, true);
+            botPlayer = new Player(PieceColor.Black, false, "medium");
+            currentPlayer = PieceColor.White;
+
+            RenderBoard();
+        }
+
+        private void RenderBoard()
+        {
+            // Render the board and pieces
+        }
+
+        private void MakeMove(Move move)
+        {
+            board.MakeMove(move);
+            RenderBoard();
+
+            currentPlayer = (currentPlayer == PieceColor.White) ? PieceColor.Black : PieceColor.White;
+
+            if (currentPlayer == botPlayer.Color)
+            {
+                Move botMove = botPlayer.MakeMove(board);
+                MakeMove(botMove);
+            }
+        }
+    }
+}
